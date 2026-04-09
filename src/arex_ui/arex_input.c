f695fe2533a22678fca64c6c41f8634555a5a8f3
YK-handsome <3074374910@qq.com>
2026-04-09 23:38:39 +0800

    editing 模式下 encoder 发的是 LV_KEY_LEFT / LV_KEY_RIGHT，不是 LV_KEY_UP / LV_KEY_DOWN。

--- src/arex_ui/arex_input.c
+++ src/arex_ui/arex_input.c
@@ -5,51 +5,44 @@
 #include "lv_drivers/win32drv/win32drv.h"
 
 /*
- * Input mapping (PC simulation):
- *   Mouse wheel / UP / DOWN  → rotate
- *   ENTER                    → click
- *   ESC / BACKSPACE          → back
- *   LEFT / RIGHT             → compass style cycle
+ * 键盘:   UP/DOWN        → rotate(-1/+1)
+ *         ENTER          → click
+ *         ESC/BACKSPACE  → back
+ * 滚轮(encoder editing模式): LEFT → rotate(-1), RIGHT → rotate(+1)
  */
-
-static lv_group_t *s_group;
 
 static void key_event_cb(lv_event_t *e)
 {
     uint32_t key = lv_event_get_key(e);
     switch (key) {
-        case LV_KEY_UP:    ui_handle_rotate(-1); break;
-        case LV_KEY_DOWN:  ui_handle_rotate(+1); break;
-        case LV_KEY_ENTER: ui_handle_click();    break;
+        case LV_KEY_UP:
+        case LV_KEY_LEFT:      ui_handle_rotate(-1); break;
+
+        case LV_KEY_DOWN:
+        case LV_KEY_RIGHT:     ui_handle_rotate(+1); break;
+
+        case LV_KEY_ENTER:     ui_handle_click();    break;
+
         case LV_KEY_ESC:
-        case LV_KEY_BACKSPACE: ui_handle_back(); break;
-        case LV_KEY_LEFT:
-            if (g_ui.state == UI_DASH) {
-                g_arex.compass.style = (g_arex.compass.style + 2) % 3;
-                arex_screen_refresh_compass_target();
-            }
-            break;
-        case LV_KEY_RIGHT:
-            if (g_ui.state == UI_DASH) {
-                g_arex.compass.style = (g_arex.compass.style + 1) % 3;
-                arex_screen_refresh_compass_target();
-            }
-            break;
+        case LV_KEY_BACKSPACE: ui_handle_back();     break;
+
         default: break;
     }
 }
 
 void arex_input_init(lv_obj_t *scr)
 {
-    s_group = lv_group_create();
-    lv_group_set_default(s_group);
-    lv_group_set_wrap(s_group, false);
+    lv_group_t *g = lv_group_create();
+    lv_group_set_default(g);
 
     lv_obj_add_flag(scr, LV_OBJ_FLAG_CLICKABLE);
     lv_obj_add_event_cb(scr, key_event_cb, LV_EVENT_KEY, NULL);
-    lv_group_add_obj(s_group, scr);
+    lv_group_add_obj(g, scr);
     lv_group_focus_obj(scr);
 
-    lv_indev_set_group(lv_win32_keypad_device_object, s_group);
-    lv_indev_set_group(lv_win32_encoder_device_object, s_group);
+    /* editing=true: encoder enc_diff → LV_KEY_LEFT/RIGHT → key_event_cb */
+    lv_group_set_editing(g, true);
+
+    lv_indev_set_group(lv_win32_keypad_device_object,  g);
+    lv_indev_set_group(lv_win32_encoder_device_object, g);
 }
