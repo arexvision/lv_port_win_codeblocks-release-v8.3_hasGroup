de7ccdd06d52109835110f641712f76debd9f9f1
Yk-handsome <3074374910@qq.com>
2026-06-12 10:46:27 +0800

    优化错误启动界面样式

--- src/error_screen/error_screen.c
+++ src/error_screen/error_screen.c
@@ -2,6 +2,7 @@
 
 #include "lvgl/lvgl.h"
 #include "ui/core/ui_defs.h"
+#include "ui/fonts/fonts.h"
 
 #if ERROR_SCREEN_ENABLED
 
@@ -20,20 +21,35 @@ static void error_screen_create(void)
     lv_obj_set_style_pad_all(scr, 0, 0);
     lv_obj_clear_flag(scr, LV_OBJ_FLAG_SCROLLABLE);
 
+    lv_coord_t group_y = (lv_coord_t)(h / 2 - 64);
+    lv_coord_t line_w = (lv_coord_t)(w > 360 ? 290 : w - 80);
+    lv_color_t title_color = lv_color_hex(0x2F6F36);
+
     lv_obj_t *title = lv_label_create(scr);
-    lv_obj_set_style_text_color(title, GREEN, 0);
-    lv_obj_set_style_text_font(title, LV_FONT_DEFAULT, 0);
+    lv_obj_set_style_text_color(title, title_color, 0);
+    lv_obj_set_style_text_font(title, FONT_HUGE, 0);
+    lv_obj_set_style_text_align(title, LV_TEXT_ALIGN_CENTER, 0);
     lv_label_set_text(title, ERROR_SCREEN_TITLE);
-    lv_obj_align(title, LV_ALIGN_CENTER, 0, -22);
+    lv_obj_align(title, LV_ALIGN_TOP_MID, 0, group_y);
+
+    lv_obj_t *line = lv_obj_create(scr);
+    lv_obj_set_size(line, line_w, 2);
+    lv_obj_set_style_bg_color(line, title_color, 0);
+    lv_obj_set_style_bg_opa(line, LV_OPA_COVER, 0);
+    lv_obj_set_style_border_width(line, 0, 0);
+    lv_obj_set_style_radius(line, 0, 0);
+    lv_obj_set_style_pad_all(line, 0, 0);
+    lv_obj_clear_flag(line, LV_OBJ_FLAG_SCROLLABLE);
+    lv_obj_align(line, LV_ALIGN_TOP_MID, 0, (lv_coord_t)(group_y + 62));
 
     lv_obj_t *message = lv_label_create(scr);
     lv_obj_set_width(message, (lv_coord_t)(w - 80));
-    lv_obj_set_style_text_color(message, lv_color_hex(0xFFFFFF), 0);
-    lv_obj_set_style_text_font(message, LV_FONT_DEFAULT, 0);
+    lv_obj_set_style_text_color(message, GREEN, 0);
+    lv_obj_set_style_text_font(message, FONT_MEDIUM, 0);
     lv_obj_set_style_text_align(message, LV_TEXT_ALIGN_CENTER, 0);
     lv_label_set_long_mode(message, LV_LABEL_LONG_WRAP);
     lv_label_set_text(message, ERROR_SCREEN_MESSAGE);
-    lv_obj_align(message, LV_ALIGN_CENTER, 0, 18);
+    lv_obj_align(message, LV_ALIGN_TOP_MID, 0, (lv_coord_t)(group_y + 76));
 
     lv_scr_load(scr);
 }
